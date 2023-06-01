813bbdcc8d7099788d252675f20a90280c667eeb
andiarwidiar1 <andiarkurnia1234@gmail.com>
2023-06-01 21:02:23 +0700

    push artikel 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import Artikel from "./pages/Artikel";
 // import { About } from "./assets/page/About";
 // import { Register } from "./assets/page/Register";
 // import { Login } from "./assets/page/Login";
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Artikel/>
       <Footer />
     </>
   );
