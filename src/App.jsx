664d7cd6f3edb098abdaa2e439ab3959dffbd317
chikaauli <chikaauliazahwa@gmail.com>
2023-06-01 17:09:35 +0700

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
+import React from "react";
 // import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
 // import { About } from "./assets/page/About";
 // import { Register } from "./assets/page/Register";
 // import { Login } from "./assets/page/Login";
 // import "./App.css";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+    <Header />
+    <Home />
+
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,85 +1,90 @@
-import "../assets/css/style.css";
-import { BsChevronDown, BsSearch } from "react-icons/bs";
-// import Logo from "../assets/img/logo.svg";
+import React from "react";
+
 
 export default function Header() {
-  return (
-    <>
-      <header id="header" className="fixed-top header-inner-pages">
-        <div className="container d-flex align-items-center justify-content-between">
-          <a href="#" className="logo">
-            <img src="src/assets/img/logo.png" alt="Remedial Logo" />
-          </a>
+    return (
+      <>
+           {/* ======= Header ======= */}
+ <header id="header" className="fixed-top header-inner-pages">
+     <div className="container d-flex align-items-center justify-content-between">
+         {/* Uncomment below if you prefer to use an image logo */}
+         <a href="index.html" className="logo">
+             <img src="src/assets/img/logo.png" alt="" className="img-fluid" />
+         </a>
 
-          <nav id="navbar" className="navbar">
-            <ul>
-              <li>
-                <a className="nav-link scrollto" href="#">
-                  Beranda
-                </a>
-              </li>
-              <li>
-                <a className="nav-link scrollto" href="#">
-                  Tentang Kami
-                </a>
-              </li>
-              <li className="dropdown ">
-                <a href="#">
-                  <span className="me-1">Program</span> <BsChevronDown />
-                </a>
-                <ul>
-                  <li>
-                    <a href="#">Kelas Ofline</a>
-                  </li>
-                  <li>
-                    <a href="#">Kelas Online</a>
-                  </li>
-                </ul>
-              </li>
-              <li className="dropdown">
-                <a href="#">
-                  <span className="me-1">Seni</span> <BsChevronDown />
-                </a>
-                <ul>
-                  <li>
-                    <a href="#">Fundamental Seni</a>
-                  </li>
-                  <li>
-                    <a href="#">Teori Seni</a>
-                  </li>
-                  <li>
-                    <a href="#">Praktik Seni</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a className="nav-link scrollto" href="#">
-                  Semua Artikel
-                </a>
-              </li>
-              <li>
-                <a className="search me-2" href="#">
-                  <BsSearch />{" "}
-                </a>
-              </li>
-              <li>
-                <a href="#" className="btn-daftar animated fadeInUp scrollto">
-                  Daftar
-                </a>
-              </li>
-              <li>
-                <a href="#" className="btn-get-started animated fadeInUp scrollto">
-                  Masuk
-                </a>
-              </li>
+         <nav id="navbar" className="navbar">
+             <ul>
+                 <li>
+                     <a className="nav-link scrollto active" href="index.html">
+                         Beranda
+                     </a>
+                 </li>
+                 <li>
+                     <a className="nav-link scrollto" href="about.html">
+                         Tentang Kami
+                     </a>
+                 </li>
+                 <li className="dropdown">
+                     <a href="#program">
+                         <span>Program</span> <i className="bi bi-chevron-down"></i>
+                     </a>
+                     <ul>
+                         <li>
+                             <a href="program_ofline.html">Kelas Ofline</a>
+                         </li>
+                         <li>
+                             <a href="program_online.html">Kelas Online</a>
+                         </li>
+                     </ul>
+                 </li>
+                 <li className="dropdown">
+                     <a href="#seni">
+                         <span>Seni</span> <i className="bi bi-chevron-down"></i>
+                     </a>
+                     <ul>
+                         <li>
+                             <a href="fundamental.html">Fundamental Seni</a>
+                         </li>
+                         <li>
+                             <a href="teori.html">Teori Seni</a>
+                         </li>
+                         <li>
+                             <a href="praktik.html">Praktik Seni</a>
+                         </li>
+                     </ul>
+                 </li>
+                 <li>
+                     <a className="nav-link scrollto" href="artikel.html">
+                         Semua Artikel
+                     </a>
+                 </li>
+                 <li>
+                     <a href="#!">
+                         <i className="bi bi-search"></i>
+                     </a>
+                 </li>
+                 <li>
+                     <a href="register.html" className="btn-daftar animated fadeInUp scrollto">
+                         Daftar
+                     </a>
+                 </li>
+                 <li>
+                     <a href="login.html" className="btn-get-started animated fadeInUp scrollto">
+                         Masuk
+                     </a>
+                 </li>
 
-              {/* <!-- <li><a href="#" className="btn-get-started animated fadeInUp scrollto">Daftar Sekarang</a></li> --> */}
-            </ul>
+                 {/* <li><a href="#!" className="btn-get-started animated fadeInUp scrollto">Daftar Sekarang</a></li>
+                 */}
+             </ul>
 
-            <i className="bi bi-list mobile-nav-toggle"></i>
-          </nav>
-        </div>
-      </header>
-    </>
+             <i className="bi bi-list mobile-nav-toggle"></i>
+         </nav>
+         {/* navbar */}
+     </div>
+ </header>
+ {/* End Header */}
+
+      </>
   );
 }
