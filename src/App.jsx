ad899a998802d5315be095ee1d18307882c7025a
ROHENDI460 <rohendi@upi.edu>
2023-06-01 21:38:56 +0700

    create router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import { About } from "./assets/page/About";
 // import { Register } from "./assets/page/Register";
 // import { Login } from "./assets/page/Login";
 // import "./App.css";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Register from "./pages/Register";
@@ -13,10 +13,19 @@ import Login from "./pages/Login";
 function App() {
   return (
     <>
-      {/* <Header />
-      <Home />
-      <Footer /> */}
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element="#" />
+          <Route path="/program-offline" element="#" />
+          <Route path="/program-online" element="#" />
+          <Route path="/fundamental-seni" element="#" />
+          <Route path="/teori-seni" element="#" />
+          <Route path="/praktik-seni" element="#" />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </>
   );
 }
