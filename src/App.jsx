de6b31ccde0738386215830755af4932026ebf4b
andiarwidiar1 <andiarkurnia1234@gmail.com>
2023-06-08 13:46:00 +0700

    perbaikan double

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,6 @@ import store from "./redux/store";
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Provider store={store}>
         <Router>
           <Header />
@@ -42,25 +41,6 @@ function App() {
           <Footer />
         </Router>
       </Provider>
-=======
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/program-offline" element={<Program_Offline />} />
-          <Route path="/program-online" element="#" />
-          <Route path="/fundamental-seni" element={<Fundamental />} />
-          <Route path="/teori-seni" element={<Teori />} />
-          <Route path="/praktik-seni" element={<Praktik />} />
-          <Route path="/artikel" element={<Artikel />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/detailartikel" element={<DetailArtikel />} />
-        </Routes>
-        <Footer />
-      </Router>
->>>>>>> ba632a2ed1b0c44317ccc32f71c870d10dc3c789
     </>
   );
 }
