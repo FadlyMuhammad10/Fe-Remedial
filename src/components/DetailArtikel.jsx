8980fcb95a8ed50a57ef3bb0afb881966cb124f4
andiarwidiar1 <andiarkurnia1234@gmail.com>
2023-06-15 21:50:36 +0700

    push push up

--- src/components/DetailArtikel.jsx
+++ src/components/DetailArtikel.jsx
@@ -8,10 +8,18 @@ export default function DetailArtikel(){
     const { id } = useParams();
     const [items, setItems] = useState([]);
 
+    const ubahDate = (date) => {
+        const newDate = new Date(date)
+        const dateStr = newDate.getDate() + "-" + newDate.getMonth() + "-" + newDate.getFullYear()
+        return dateStr
+    }
+
     useEffect(() => {
-        fetch("https://64513164e1f6f1bb22aab5b3.mockapi.io/artikel/" +id)
+        fetch("https://be4-skilvul-production.up.railway.app/api/v1/detailpage/artikel/" + id)
         .then((res) => res.json())
-        .then((data) => setItems(data));
+        .then((data) => {
+            setItems(data.data)
+        });
     }, []);
     console.log(items)
     return(
@@ -41,16 +49,16 @@ export default function DetailArtikel(){
                             <div className="col-11">
                                 <div className="row justify-content-center align-items-center">
                                     <div className="col-7 col-lg-9 col-xl-9 col-xxl-8 text-white">
-                                        <h5 className="pt-3">{items.kategori}</h5>
-                                        <h1>{items.title}</h1>
+                                        <h5 className="pt-3">{items?.kategori?.nama_kategori}</h5>
+                                        <h1>{items?.title}</h1>
                                         <div className="col d-flex">
-                                            <p className="ps-1">oleh {items.author}</p>
-                                            <p className="ps-3">{items.date}</p>
+                                            <p className="ps-1">oleh {items?.author}</p>
+                                            <p className="ps-3">{ubahDate(items?.date)}</p>
                                         </div>
                                     </div>
 
                                     <div className="col-4 col-lg-3 col-xl-3 col-xxl-4 d-flex justify-content-end">
-                                        <img className="img-fluid" src={items.image} alt=""/>
+                                        <img className="img-fluid" src={items?.image_url} alt=""/>
                                     </div>
                                 </div>
                             </div>
@@ -60,13 +68,13 @@ export default function DetailArtikel(){
                         <div className="col-10 col-lg-11">
                             <div className="row justify-content-center">
                                 <div className=" col-md-12 col-lg-9 col-xl-9 col-xxl-9">
-                                    {/* <p className="mt-4">Seni rupa murni adalah seni rupa yang mengutamakan keindahan dan keunikan karyanya, tanpa memperhatikan fungsi dari benda yang diciptakannya. Dalam bahasa inggris, seni murni disebut “fine art” yang secara harfiah berarti “seni mendetail”. Detail di sini berarti karya seni yang dibuat sangat memperhatikan detail-detail kecil nilai estetis yang jarang diperhatikan oleh perupa pada umumnya</p>
-                                    <p>Perupa seni lain bisa saja meninggalkan detail kecil nilai estetis untuk mengikuti keinginan klien atau kebutuhan pasar. Sementara seniman murni tidak harus melakukannya, meskipun pada akhirnya seni murni juga akan menciptakan pasarnya sendiri dan membutuhkan sedikit konsolidasi. Intinya, jika senimannya berkehendak, ia dapat benar-benar menanggalkan seluruh aspek komersil dan manfaat dari karya yang ia ciptakan.</p>
-                                    <h5 className="fw-bold">Batasan Seni Murni</h5>
-                                    <p>Namun bukan berarti perupa murni dapat meracau seenaknya menciptakan benda yang menurut ia adalah seni. Bagi kalangan awam, beberapa seniman murni mungkin tampak seperti itu. Kenyataannya, banyak tantangan yang harus dihadapi ketika seseorang menciptakan karya seni murni.Karya seni murni harus mendapatkan pengakuan dari para penggiat dunia seni yang akan mengapresiasi, mengkaji, dan mengkritik karya seni. Para penggiat dunia seni tersebut meliputi: kritikus seni, akademisi, kurator, pemilik galeri, perupa murni lain, hingga para kolektor yang akan membeli karya mereka. Sebetulnya masyarakat umum juga merupakan bagian dari lingkaran sosial seni, namun seorang perupa murni dapat mengabaikan satu atau dua, bahkan semua lingkaran medan seni tersebut.</p>
-                                    <h5 className="fw-bold">Perbedaan Seni Rupa Murni dan Seni Rupa Terapan</h5>
-                                    <p>Sebelumnya, telah dijelaskan bahwa seni rupa murni tidak memperhatikan nilai guna dari karya yang dibuat. Sebaliknya, terdapat seni yang mengutamakan fungsi di atas nilai estetis dan biasa disebut sebagai seni rupa terapan (applied art) atau desain. Seni rupa terapan mengutamakan fungsi dari benda seni yang diciptakan dan menghasilkan benda seni seperti pakaian, arsitektur gedung, benda dekorasi, dsb.</p>
-                                    <hr/>     */}
+                                    <p className="mt-4">{items?.pengertian}</p>
+                                    <p>{items?.artikel_web}</p>
+                                    <h5 className="fw-bold">{items?.subbab1}</h5>
+                                    <p>{items?.artikel1}</p>
+                                    <h5 className="fw-bold">{items?.subbab2}</h5>
+                                    <p>{items?.artikel2}</p>
+                                    <hr/>    
                                 </div>
 
                                 <div className="col-12 col-lg-3 col-xl-3 col-xxl-3">
